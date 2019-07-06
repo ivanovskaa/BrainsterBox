196e596bbc18aa03c47dffbe969ae500b5674ac1
A.I <ivanovskaaleksandra@hotmail.com>
2019-07-06 09:20:16 +0200

    local changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Container, Row} from 'react-bootstrap';
+import LandingPage from './Components/LandingPage/LandingPage';
+import GameDescription from './Components/Game/GameCont/GameDescription/GameDescription';
+import AboutUs from './Components/AboutUs/AboutUs';
+import ContactUs from './Components/Contact/ContactUs';
+import Register from './Components/Register/Register';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Container fluid>
+            <Row> 
+                <Switch>
+                  <Route exact path="/" component={LandingPage} />
+                  <Route path="/GameDescription/:id"  component={GameDescription}/>
+                  <Route path="/AboutUs" component={AboutUs}/>
+                  <Route path="/ContactUs" component={ContactUs}/>
+                  <Route path="/Registration" component={Register}/>
+                </Switch>
+            </Row>
+          </Container>       
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
+
