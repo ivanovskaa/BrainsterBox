380a1eff0c3f9d3f2dc61b570f6a8a47ae59f680
Aleksandar Sajdovski <Aleksandar.Sajdovski@gmail.com>
2019-07-22 11:40:58 +0200

    Updates

---
+++ precache-manifest.f61b472874a4d17bb54ff68079eff12b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1968da752326789f90a40b2b621d7fbf",
+    "url": "/BrainsterBox/index.html"
+  },
+  {
+    "revision": "1a1817d9d2f39ae74c36",
+    "url": "/BrainsterBox/static/css/main.9239040b.chunk.css"
+  },
+  {
+    "revision": "9f163cf77bb4f933ad3c",
+    "url": "/BrainsterBox/static/js/2.baf4b151.chunk.js"
+  },
+  {
+    "revision": "1a1817d9d2f39ae74c36",
+    "url": "/BrainsterBox/static/js/main.fca3db43.chunk.js"
+  },
+  {
+    "revision": "cc6679132648dda4f6bc",
+    "url": "/BrainsterBox/static/js/runtime~main.8e6b4d05.js"
+  },
+  {
+    "revision": "0f4ef4f15b0685c10671288c4cb62a9f",
+    "url": "/BrainsterBox/static/media/Map.0f4ef4f1.jpg"
+  },
+  {
+    "revision": "dcd3727437d20b4817cd5c9c7787717b",
+    "url": "/BrainsterBox/static/media/logo.dcd37274.png"
+  }
+]);
